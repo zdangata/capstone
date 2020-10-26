c5d30913173028d71d4c62d6f3b57859a5846fc0
zdangata <zdangata@gmail.com>
2020-10-26 23:30:02 +0000

    my message

--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'fsndp.eu.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'https://localhost:5000'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 ## AuthError Exception
 '''
